0fa94b768d091b990c849af969f2cd544dd2db44
vishnuchithravishnu-afk <vishnuchithravishnu@gmail.com>
2026-08-13 20:20:03 +0530

    Add files via upload

---
+++ task 4.py
@@ -0,0 +1,10 @@
+year = int(input("Enter year: "))
+
+if year % 400 == 0:
+    print("Leap Year")
+elif year % 100 == 0:
+    print("Not a Leap Year")
+elif year % 4 == 0:
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
